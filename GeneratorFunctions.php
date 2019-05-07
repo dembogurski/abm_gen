53a2ddafb17ef5b55acf331b3e6a0ddacf16cb5b
dembogurski <dembogurski@gmail.com>
2019-05-07 07:51:06 -0400

    First Commit

---
+++ GeneratorFunctions.php
@@ -0,0 +1,130 @@
+<?php
+
+class GeneratorFunctions {
+
+    function __construct() {
+        $action = $_REQUEST['action'];
+        switch ($action) {
+            case '': $this->home();
+                break;
+            case 'home': $this->home();
+                break;
+            case 'get_databases': $this->getDatabases();
+                break;
+            case 'get_tables': $this->getTables();
+                break;
+            case 'get_columns': $this->getColumns();
+                break;
+            case 'get_element_attributes': $this->getElementAttributes();
+                break;
+             
+            default : $this->home();
+        }
+    }
+
+    function getIP() {
+        if (isset($_SERVER)) {
+            if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+                return $_SERVER['HTTP_X_FORWARDED_FOR'];
+            } else {
+                return $_SERVER['REMOTE_ADDR'];
+            }
+        } else {
+            if (isset($GLOBALS['HTTP_SERVER_VARS']['HTTP_X_FORWARDER_FOR'])) {
+                return $GLOBALS['HTTP_SERVER_VARS']['HTTP_X_FORWARDED_FOR'];
+            } else {
+                return $GLOBALS['HTTP_SERVER_VARS']['REMOTE_ADDR'];
+            }
+        }
+    }
+
+    function getDatabases() {
+        include_once ( "Y_DB.class.php" );
+        $user = $_REQUEST['user'];
+        $pass = $_REQUEST['pass'];
+
+        $db = new Y_DB();
+        $db->User = $user;
+        $db->Password = $pass;
+        try {
+            $db->Query("show databases");
+
+            echo '<select id="databases" onchange="getDBTables()">';
+            echo "<option value='Seleccione'>Seleccione</option>";
+            while ($db->NextRecord()) {
+                $data = $db->Record['Database'];
+                echo "<option value='$data'>$data</option>";
+            }
+            echo '</select>';
+            echo "<span id='tables_sp'></span>";
+        } catch (Exception $e) {
+            echo "No se puede conectar, Usuario u Contrase&ntilde;a incorrectos...";
+        }
+    }
+
+    function getTables() {
+        include_once ( "Y_DB.class.php" );
+        $user = $_REQUEST['user'];
+        $pass = $_REQUEST['pass'];
+        $database_name = $_REQUEST['db'];
+
+        $db = new Y_DB();
+        $db->User = $user;
+        $db->Password = $pass;
+
+        $db->Query("SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA LIKE '$database_name' ");
+        echo '<label>&nbsp;<b>Tabla:</b>&nbsp;</label>  <select id="tables" onchange="getColumns()">';
+        echo "<option value='Seleccione'>Seleccione  la Tabla</option>";
+        while ($db->NextRecord()) {
+            $table = $db->Record['TABLE_NAME'];
+            echo "<option value='$table'>$table</option>";
+        }
+        echo '</select>';
+    }
+
+    function getColumns() {
+        include_once ( "Y_DB.class.php" );
+        require_once('Y_Template.class.php');
+        $t = new Y_Template("GeneratorFunctions.html");
+        $user = $_REQUEST['user'];
+        $pass = $_REQUEST['pass'];
+        $database_name = $_REQUEST['db'];
+        $table_name = $_REQUEST['table'];
+
+        $db = new Y_DB();
+        $db->User = $user;
+        $db->Password = $pass;
+
+        $db->Query("SELECT  COLUMN_NAME, IS_NULLABLE, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, NUMERIC_PRECISION  FROM information_schema.COLUMNS c WHERE   TABLE_SCHEMA LIKE '$database_name' AND TABLE_NAME LIKE '$table_name'  ");
+
+
+        $t->Show("cabecera");
+
+        while ($db->NextRecord()) {
+            $id = $db->Record['COLUMN_NAME'];
+            $isn = $db->Record['IS_NULLABLE'];
+            $dt = $db->Record['DATA_TYPE'];
+            $cml = $db->Record['CHARACTER_MAXIMUM_LENGTH'];
+            $np = $db->Record['NUMERIC_PRECISION'];
+
+            $t->Set('id', $id);
+            $t->Set('isn', $isn);
+            $t->Set('dt', $dt);
+            $t->Set('cml', $cml);
+            $t->Set('np', $np);
+            $t->Show("datos");
+            // echo  "<tr> <td><input type='checkbox' name='seleccionados' value='id' /></td> <td>$cn</td>  <td>$isn</td> <td>$dt</td>  <td>$cml</td>  <td>$np</td>";
+        }
+        $t->Show("fin");
+    }
+    
+    
+    function getElementAttributes(){
+        echo 'value=".............."';
+    }
+
+}
+
+new GeneratorFunctions();
+?>    
+
---
+++ index.php
@@ -0,0 +1,161 @@
+<html>
+    <head>
+        <script src="js/jquery_2.1.0.js"></script>
+
+
+        <script language="javascript">
+            
+             var db = null;
+             var table =null;
+             var host = null;
+             var user =null;
+             var pass =null;
+            
+            
+            function connect(){
+                var host = $("#db_host").val();
+                var user = $("#db_user").val();
+                var pass = $("#db_pass").val();
+                
+                $.ajax({
+                        type: "POST",
+                        url: "http://"+host+"/abm_gen/GeneratorFunctions.php",
+                        data: "action=get_databases&user="+user+"&pass="+pass+"",
+                        async:true,
+                        dataType: "html",
+                        beforeSend: function(objeto){
+                            $("#msg").html("<img src='images/activity.gif' width='30' height='11' >");
+                        },
+                        complete: function(objeto, exito){
+                            if(exito=="success"){ 
+                                   $("#db"  ).html(objeto.responseText );  
+                                   $("#msg"  ).html("");  $("#barra"  ).html("");  
+                            }
+                        }
+                    });                  
+                
+            }
+            
+            function getDBTables(){
+                  db = $("#databases").val();
+                  host = $("#db_host").val();
+                  user = $("#db_user").val();
+                  pass = $("#db_pass").val();
+                
+                $.ajax({
+                        type: "POST",
+                        url: "http://"+host+"/abm_gen/GeneratorFunctions.php",
+                        data: "action=get_tables&db="+db+"&user="+user+"&pass="+pass+"",
+                        async:true,
+                        dataType: "html",
+                        beforeSend: function(objeto){
+                            $("#msg").html("<img src='images/activity.gif' width='30' height='11' >");
+                        },
+                        complete: function(objeto, exito){
+                            if(exito=="success"){ 
+                                   $("#tables_sp"  ).html(objeto.responseText );  
+                                   $("#msg"  ).html("");  $("#barra"  ).html("");  
+                            }
+                        }
+                    }); 
+            }
+            function getColumns(){
+                var db = $("#databases").val();
+                var table = $("#tables").val();
+                var host = $("#db_host").val();
+                var user = $("#db_user").val();
+                var pass = $("#db_pass").val();
+                
+                $.ajax({
+                        type: "POST",
+                        url: "http://"+host+"/abm_gen/GeneratorFunctions.php",
+                        data: "action=get_columns&db="+db+"&table="+table+"&user="+user+"&pass="+pass+"",
+                        async:true,
+                        dataType: "html",
+                        beforeSend: function(objeto){
+                            $("#msg").html("<img src='images/activity.gif' width='30' height='11' >");
+                        },
+                        complete: function(objeto, exito){
+                            if(exito=="success"){ 
+                                   $("#columns"  ).html(objeto.responseText );  
+                                   $("#msg"  ).html("");  $("#barra"  ).html("");  
+                            }
+                        }
+                    }); 
+            }
+            
+            function getAttributes(id){  
+              // var type = $("#types_"+id+" option:selected").text();
+              var type = $("#types_"+id ).val(); alert(type);
+                
+                $.ajax({
+                        type: "POST",
+                        url: "http://"+host+"/abm_gen/GeneratorFunctions.php",
+                        data: "action=get_element_attributes&type="+type,
+                        async:true,
+                        dataType: "html",
+                        beforeSend: function(objeto){
+                            $("#msg").html("<img src='images/activity.gif' width='30' height='11' >");
+                        },
+                        complete: function(objeto, exito){
+                            if(exito=="success"){ 
+                                   //Levantar Popup o Ventana
+                                  alert(objeto.responseText);
+                                   //$("#xxx"  ).html(objeto.responseText );  
+                                   $("#msg"  ).html("");   
+                            }
+                        }
+                    }); 
+            }
+            
+  
+        </script>    
+        <style>
+            th{
+                text-align: left;
+            }
+            .mlistado{
+                border:1px #E5E5E5 solid;
+                border-collapse:collapse;
+            }
+        </style>
+
+    </head>
+
+    <body>
+        <div >
+
+
+            <input type="hidden" name="paso" value="1" />
+            <table class="mformulario">
+                <thead>
+                    <tr>
+                        <th colspan="2">Conectar a la base de datos</th>
+                    </tr>
+                </thead>
+                <tr>
+                    <th>Host:</th>
+                    <td><input type="text" id="db_host" value="localhost"  />   <span id="msg"> </span></td>
+                </tr>
+                <tr>
+                    <th>Usuario:</th>
+                    <td><input type="text" id="db_user" value="root" /></td>
+                </tr>
+                <tr>
+                    <th>Contraseña:</th>
+                    <td><input type="text" id="db_pass" /></td>
+                </tr>
+                <tr>
+                    <th>Base de Datos</th>
+                    <td id="db"></td>
+                </tr> 
+                <tr>
+                    <td colspan="2" align="right" id="barra"><input type="button" onclick="connect()" value="Conectar >" />   </td>
+                </tr>
+            </table>
+
+
+        </div>
+        <div id='columns' style="padding-left: 10px;padding-top: 10px" ></div> 
+    </body>
+</html>
